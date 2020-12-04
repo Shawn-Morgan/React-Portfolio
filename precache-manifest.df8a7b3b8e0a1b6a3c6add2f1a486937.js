691f9185797457eb1772c2e49485dc2e9f222d1a
shawnm7474 <shawnm74@gmail.com>
2020-12-03 20:30:12 -0600

    Updates

---
+++ precache-manifest.df8a7b3b8e0a1b6a3c6add2f1a486937.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6fced77042f2ce434d24c4169f0649",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "9ae599c2b3808f3e229e",
+    "url": "/React-Portfolio/static/css/2.e18a25e7.chunk.css"
+  },
+  {
+    "revision": "bea097bd665e056999f1",
+    "url": "/React-Portfolio/static/css/main.a1b7b015.chunk.css"
+  },
+  {
+    "revision": "9ae599c2b3808f3e229e",
+    "url": "/React-Portfolio/static/js/2.1db567f4.chunk.js"
+  },
+  {
+    "revision": "bea097bd665e056999f1",
+    "url": "/React-Portfolio/static/js/main.4196a889.chunk.js"
+  },
+  {
+    "revision": "a5982163e5c3e3252977",
+    "url": "/React-Portfolio/static/js/runtime~main.982e8b01.js"
+  },
+  {
+    "revision": "22546d92b637106f8722a2a0084071fe",
+    "url": "/React-Portfolio/static/media/CritterSitterLogo-V.22546d92.png"
+  },
+  {
+    "revision": "215dfba489f57015c764a88f6d32b16d",
+    "url": "/React-Portfolio/static/media/fitness.215dfba4.jpg"
+  },
+  {
+    "revision": "212b68d626d6cecfc73621cc229abae9",
+    "url": "/React-Portfolio/static/media/notetaker.212b68d6.png"
+  },
+  {
+    "revision": "8ddc9d0ec7f4d11556e09dff5923693e",
+    "url": "/React-Portfolio/static/media/quiz2.8ddc9d0e.jfif"
+  },
+  {
+    "revision": "d8a0828c9ebf73cd45369b75dcb6799a",
+    "url": "/React-Portfolio/static/media/space.d8a0828c.jfif"
+  },
+  {
+    "revision": "1791da2d5fac145207e6665c5bd9291a",
+    "url": "/React-Portfolio/static/media/weather.1791da2d.jfif"
+  }
+]);
